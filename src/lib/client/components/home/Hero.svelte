5a8e2745b4ef9d7a2df6350692bb3c44091820e1
Lance <lancehemphill2000@gmail.com>
2025-07-21 17:55:42 -0400

    removed outlines from cards, changed hero image

--- src/lib/client/components/home/Hero.svelte
+++ src/lib/client/components/home/Hero.svelte
@@ -2,6 +2,6 @@
 </script>
 
 <section class="relative flex w-full items-center overflow-hidden h-1/3">
-	<img src="/images/nether.png" alt="" class="absolute inset-0 -z-10 object-center" />
+	<img src="/images/field.png" alt="" class="absolute inset-0 -z-10 object-center" />
 	<img src="/images/virarith.png" alt="" class="mx-auto w-[35rem] bg-transparent px-4" />
 </section>
